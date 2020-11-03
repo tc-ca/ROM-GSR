ab75ad54c7430e94c3bf6a37f8505b68851c5c11
hongliutc <72039664+hongliutc@users.noreply.github.com>
2020-11-03 09:34:24 -0500

    Hide Show Operation Tabs on Account form

--- WebResources/OversightScripts/src/account.ts
+++ WebResources/OversightScripts/src/account.ts
@@ -4,12 +4,21 @@ namespace ROM.Account {
     export function onLoad(eContext: Xrm.ExecutionContext<any, any>): void {
       console.log('Onload has been called');
       console.log('Xrm execution context: ', eContext);
-      const form = <Form.account.Main.Organizations>eContext.getFormContext();
+      hideShowOperations(eContext);
+    }
+
+    export function hideShowOperations(eContext: Xrm.ExecutionContext<any, any>): void {
+        console.log('hideShowOperations has been called');
+        console.log('Xrm execution context: ', eContext);
+        const form = <Form.account.Main.Organizations>eContext.getFormContext();
         var accountType = form.getAttribute('customertypecode').getValue();
         if (accountType == account_customertypecode.RegulatedEntity){
         form.getControl('regulated_entities_subgrid').setVisible(false);
-        }
-        else
-        form.getControl('regulated_entities_subgrid').setVisible(true);
-    }
+        form.getControl('sites_subgrid').setVisible(true);
+        };
+        if (accountType == account_customertypecode.Site){
+            form.getControl('regulated_entities_subgrid').setVisible(true);
+            form.getControl('sites_subgrid').setVisible(false);
+        };
+      }
 }
