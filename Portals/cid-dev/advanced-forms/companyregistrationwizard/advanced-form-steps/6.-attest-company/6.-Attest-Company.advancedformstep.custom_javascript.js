df805913d37de952ef56b534ed6a198eee3a04b9
tc-buildbot <nepasrepondre-noreply@tc.gc.ca>
2022-02-09 22:05:19 +0000

    Update portal

---
+++ Portals/cid-dev/advanced-forms/companyregistrationwizard/advanced-form-steps/6.-attest-company/6.-Attest-Company.advancedformstep.custom_javascript.js
@@ -0,0 +1,39 @@
+// CompanyRegistrationWizard-Attestation.js
+
+$(document).ready(function () {
+    $("#cid_crabusinessnumber").attr("placeholder", "");
+    $("#cid_reasonfornobnnumber").attr("placeholder", "");
+    $("#cid_reasonfornobnnumber_other").attr("placeholder", "");
+    $("#address1_line2").attr("placeholder", "");
+    $("#address1_line3").attr("placeholder", "");
+    $("#telephone1").attr("placeholder", "");
+    $("#fax").attr("placeholder", "");
+    $("#websiteurl").attr("placeholder", "");
+
+    debugger;
+    //var cid_crabusinessnumber = '{{user.cid_crabusinessnumber}}';
+
+    var cid_crabusinessnumber = $("#cid_crabusinessnumber").val();
+    cid_crabusinessnumber = (cid_crabusinessnumber != "null" ? cid_crabusinessnumber : "");
+
+    // do not have a business number?
+    if (cid_crabusinessnumber == "") {
+        $("#cid_crabusinessnumber").parent().parent().hide();
+        $("#cid_reasonfornobnnumber").parent().parent().show();
+        $("#cid_reasonfornobnnumber_other").parent().parent().hide();
+    }
+    else {
+        $("#cid_crabusinessnumber").parent().parent().show();
+        $("#cid_reasonfornobnnumber").parent().parent().hide();
+        $("#cid_reasonfornobnnumber_other").parent().parent().hide();
+    }
+
+    $('table').each(function () {
+        var selectedTable = $(this);
+        if (selectedTable.attr('data-name').includes('_readonly')) {
+            selectedTable.find("tr").each(function () {
+                $(this).css("background-color", "#F0F0F0");
+            });
+        }
+    });
+});
