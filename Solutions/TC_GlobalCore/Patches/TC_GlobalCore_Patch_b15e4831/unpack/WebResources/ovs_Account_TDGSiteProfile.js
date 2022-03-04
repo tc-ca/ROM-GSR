e4f1f2e89513a2ed26bceea4dad57830bd4bb96e
tc-buildbot <nepasrepondre-noreply@tc.gc.ca>
2022-03-04 14:22:09 +0000

    Update Solution - TC_GlobalCore_Patch_b15e4831:

---
+++ Solutions/TC_GlobalCore/Patches/TC_GlobalCore_Patch_b15e4831/unpack/WebResources/ovs_Account_TDGSiteProfile.js
@@ -0,0 +1,53 @@
+﻿var Account_TDGSiteProfile = (function (window, document) {
+
+    var formMapping_EN = {
+        "948010001": "TDG Site Profile",
+        "948010000": "TDG Organizations",
+    };
+
+    var formMapping_FR = {
+        "948010001": "Profil du site de TMD",
+        "948010000": "Organisations TMD",
+    };
+    //const formContextGlobalRef : any;
+
+    const TAB_NAME = "tab_Operations"
+    const SECTION_NAME = "tab_Operations_section_NOP_Plaaning";
+    const isInspectorOrAnalyst = true;
+    const isPlannerOrManager = true;
+
+    function filter_customertypecode(formContext) {
+
+        const options = new Array("948010000","948010001");
+        glHelper.filterOptionSet(formContext, "customertypecode", options, true);
+
+    }
+
+    function setFieldPermissions(formContext) {
+        const rType = glHelper.GetValue(formContext, "customertypecode");
+       
+        if (rType !== null && rType.toString() === "948010001") {
+           
+            if(isInspectorOrAnalystOrManager === (glHelper.hasCurrentUserRole("TDG Inspector")|| glHelper.hasCurrentUserRole("TDG Analyst") ||glHelper.hasCurrentUserRole("TDG Manager"))){
+                glHelper.SetSectionVisibility(formContext, TAB_NAME, SECTION_NAME, false);
+            }
+           
+            if(isPlanner === (glHelper.hasCurrentUserRole("TDG Planner"))) {
+                glHelper.SetSectionVisibility(formContext, TAB_NAME, SECTION_NAME, true);
+            }
+
+        }
+    }
+
+
+    return{
+
+        OnLoad: function (executionContext) {
+
+            const formContextGlobalRef = executionContext.getFormContext();
+
+            setFieldPermissions(formContextGlobalRef);
+        }
+    }
+    
+})(window, document);
