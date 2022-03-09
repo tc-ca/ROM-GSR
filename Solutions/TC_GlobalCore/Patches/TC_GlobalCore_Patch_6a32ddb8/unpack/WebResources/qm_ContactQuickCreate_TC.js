2bc62a54c51ea2e77b678259100c0ac1842fb692
tc-buildbot <nepasrepondre-noreply@tc.gc.ca>
2022-03-09 06:08:15 +0000

    Update Solution - TC_GlobalCore_Patch_6a32ddb8:

---
+++ Solutions/TC_GlobalCore/Patches/TC_GlobalCore_Patch_6a32ddb8/unpack/WebResources/qm_ContactQuickCreate_TC.js
@@ -0,0 +1,33 @@
+﻿///<reference path="../../Utilities/GlobalHelper.js"/>
+///<reference path="../../Utilities/questionnaireFunctions.js"/>
+
+var Contact_QC_TC= (function (window, document) {
+
+
+    return {
+
+        OnLoad: function (executionContext) {
+            
+            var formContext = executionContext.getFormContext();
+
+            // Set Account as Required field.
+            glHelper.SetRequiredLevel(formContext, "firstname", true);
+            glHelper.SetRequiredLevel(formContext, "lastname", true);
+            glHelper.SetRequiredLevel(formContext, "parentcustomerid", true);
+            glHelper.SetRequiredLevel(formContext, "emailaddress1", true);
+            glHelper.SetRequiredLevel(formContext, "jobtitle", true);
+            glHelper.SetRequiredLevel(formContext, "telephone1", true);
+            glHelper.SetRequiredLevel(formContext, "parentcustomerid", true);
+            
+            if (window.top.QuickCreateHelper != null && window.top.QuickCreateHelper != undefined
+                && window.top.QuickCreateHelper.site != null && window.top.QuickCreateHelper != undefined) {
+
+                try {
+                    glHelper.SetLookup(formContext, "parentcustomerid", window.top.QuickCreateHelper.site.et, window.top.QuickCreateHelper.site.id, window.top.QuickCreateHelper.site.name);
+                    glHelper.SetDisabled(formContext, "parentcustomerid", true);
+                } catch {}
+            }
+        },
+    }
+
+})(window, document)
