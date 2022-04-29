40a86848f8ff615188c115dea95a78565fe6e2ae
tc-buildbot <nepasrepondre-noreply@tc.gc.ca>
2022-04-29 22:02:27 +0000

    Update portal

--- Portals/cid-dev/web-pages/company-history-log/content-pages/Company-History-Log.en-US.webpage.custom_javascript.js
+++ Portals/cid-dev/web-pages/company-history-log/content-pages/Company-History-Log.en-US.webpage.custom_javascript.js
@@ -5,10 +5,4 @@ $(document).ready(function () {
 	if(companyName)
 		if(topNav)
 			$( "<h2>TDG Site Registration Database: " + companyName + "</h2>" ).insertAfter(topNav);
-
-	$("legend").each(function() {
-		$(this).removeClass();
-		$(this).css("font-size", "24px");
-		$(this).css("font-weight", "bold");
-    });
 });
