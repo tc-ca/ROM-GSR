e5125b55f5c4a6096f7f9d7d2f10ba37debc60c0
gitName <a@b.c>
2022-07-20 10:38:38 -0400

    update filenames

---
+++ ConfigurationMigration/scripts/Export-CrmDataFile.ps1
@@ -0,0 +1,9 @@
+﻿$basepath = "C:/Users/leblaaa/source/repos/ROM-GSR/ConfigurationMigration/"
+$schemaFile = "$basepath/schema/CostRecoveryAttributes.xml"
+$dataFile = "$basepath/data/CostRecoveryAttributes.zip"
+$crmConnection = "AuthType=ClientSecret; url=https://tdgcore-dev-tcd365.crm3.dynamics.com; ClientId=a6adc57f-f130-4e0e-8cab-c0d6a26f8273; ClientSecret=2e5L2VU.an8-Smpcn.nu2wavk__sY5eocF"
+$logWriteDirectory = "$basepath\logs\export\"
+
+Install-Module -Name Microsoft.Xrm.Tooling.ConfigurationMigration -force
+
+Export-CrmDataFile -SchemaFile $schemaFile -DataFile $dataFile -CrmConnection $crmConnection -Verbose -EmitLogToConsole -LogWriteDirectory $logWriteDirectory
---
+++ ConfigurationMigration/scripts/Repack-CrmDataFile.ps1
@@ -0,0 +1,16 @@
+﻿Install-Module -Name Xrm.Framework.CI.PowerShell.Cmdlets -Force
+
+$basepath = "C:/Users/leblaaa/source/repos/ROM-GSR/ConfigurationMigration"
+
+$combineDataXmlFile = $true
+$dataZip = "$basepath/repacked/CostRecoveryAttributes.zip"
+$extractFolder = "$basepath/extract"
+
+If(!(test-path "$basepath/repacked"))
+{
+    Write-Host "$basepath/repacked folder does not exists."
+    New-Item -ItemType Directory -Force -Path "$basepath/repacked"
+    Write-Host "$basepath/repacked folder created."
+}
+
+Compress-XrmCMData -DataZip "$dataZip" -Folder "$extractFolder" -CombineDataXmlFile $combineDataXmlFile
