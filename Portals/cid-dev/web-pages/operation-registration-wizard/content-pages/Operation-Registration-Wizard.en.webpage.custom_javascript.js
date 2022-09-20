b95a38551f620dc2dbf0b0bacb17b5f1738ffb4a
tc-buildbot <nepasrepondre-noreply@tc.gc.ca>
2022-09-20 18:24:19 +0000

    Update portal

--- Portals/cid-dev/web-pages/operation-registration-wizard/content-pages/Operation-Registration-Wizard.en.webpage.custom_javascript.js
+++ Portals/cid-dev/web-pages/operation-registration-wizard/content-pages/Operation-Registration-Wizard.en.webpage.custom_javascript.js
@@ -60,7 +60,7 @@ function header_setup() {
     sessionStorage.setItem("selected_language", selected_language);
 
     try {
-        var code = "m000025";
+        var code = "m000024";
         var companyName = '{{user.parentcustomerid.name}}';
         var value = tdg.error_message.message(code);
         value = value.replace("{0}", companyName);
