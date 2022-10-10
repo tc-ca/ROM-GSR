f97ca7bd0efbd2653d5f1217b1243adb67781c88
tc-buildbot <nepasrepondre-noreply@tc.gc.ca>
2022-10-10 08:02:46 +0000

    Update portal

--- Portals/cid-dev/web-pages/dashboard_5953ec76/content-pages/Dashboard_5953ec76.en.webpage.custom_javascript.js
+++ Portals/cid-dev/web-pages/dashboard_5953ec76/content-pages/Dashboard_5953ec76.en.webpage.custom_javascript.js
@@ -5,6 +5,18 @@
 $(document).ready(function () {
     debugger;
 
+    var urlParams = new URLSearchParams(window.location.search);
+	if (urlParams.has('firsttime')) {
+        var firstTime = urlParams.get('firsttime')
+
+        if(firstTime == "true"){
+            message = "The registration of your Company in the CID Data Platform is now complete. " +
+            "An email has been sent to your address, confirming the completion of the registration." +
+            "<br><br>You are currently in the initial in-year page, which will be your starting page for each future CID session.";//tdg.error_message.message("m000000");
+	        tdg.c.dialog_OK(message);
+        }
+    }
+
     var topNav = $('#navbar');
     if (topNav) {
         var companyName = $("#ovs_legalname").val();
--- Portals/cid-dev/web-pages/registration-completion/content-pages/Registration-Completion.en.webpage.custom_javascript.js
+++ Portals/cid-dev/web-pages/registration-completion/content-pages/Registration-Completion.en.webpage.custom_javascript.js
@@ -4,8 +4,9 @@ $(document).ready(function () {
     var pageURL = window.location.href;
 
     if(pageURL.includes("tdgcore-dev") || pageURL.includes("tdgcore-qa")){
-        $("#div1").hide();
-        $("#div2").show();
+        //$("#div1").hide();
+        //$("#div2").show();
+        window.location.href = "~/company_dashboard/?firsttime=true";
     }
     else{
         $("#div1").show();
