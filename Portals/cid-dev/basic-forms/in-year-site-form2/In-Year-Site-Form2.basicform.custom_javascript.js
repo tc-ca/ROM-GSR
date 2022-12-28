e4e5ac50d5b804d335d122f90c749dfec4dec693
tc-buildbot <nepasrepondre-noreply@tc.gc.ca>
2022-12-28 22:02:27 +0000

    Update portal

--- Portals/cid-dev/basic-forms/in-year-site-form2/In-Year-Site-Form2.basicform.custom_javascript.js
+++ Portals/cid-dev/basic-forms/in-year-site-form2/In-Year-Site-Form2.basicform.custom_javascript.js
@@ -6,7 +6,76 @@ var _busy = false;
 
 $(document).ready(function () {
     debugger;
-   
+    var CompanyName = '{{user.parentcustomerid.name}}';
+    //get site id
+    var urlParams = new URLSearchParams(window.location.search);
+    if (urlParams.has('id')) {
+        var recodId = urlParams.get('id');
+        var SiteAddressInfo = tdg.webapi.SelectedColumnlist("accounts",
+        "cid_sitename,ovs_address_type,address1_line1,address1_city,ovs_address1_province,address1_stateorprovince,address1_latitude,address1_longitude,ovs_lld_quarter,ovs_lld_section,ovs_lld_township,ovs_lld_range,ovs_lld_meridian",
+        "accountid eq " + recodId);
+        var cid_sitename = SiteAddressInfo[0]["cid_sitename"];
+        var ovs_address_type = SiteAddressInfo[0]["ovs_address_type"];
+        var address1_line1 = SiteAddressInfo[0]["address1_line1"];
+        var address1_city = SiteAddressInfo[0]["address1_city"];
+        var address1_stateorprovince = SiteAddressInfo[0]["address1_stateorprovince"];
+        var address1_latitude = SiteAddressInfo[0]["address1_latitude"];
+        var address1_longitude = SiteAddressInfo[0]["address1_longitude"];
+        var ovs_lld_quarter = SiteAddressInfo[0]["ovs_lld_quarter"];
+        var ovs_lld_section = SiteAddressInfo[0]["ovs_lld_section@OData.Community.Display.V1.FormattedValue"];
+        var ovs_lld_township = SiteAddressInfo[0]["ovs_lld_township@OData.Community.Display.V1.FormattedValue"];
+        var ovs_lld_range = SiteAddressInfo[0]["ovs_lld_range@OData.Community.Display.V1.FormattedValue"];
+        var ovs_lld_meridian = SiteAddressInfo[0]["ovs_lld_meridian@OData.Community.Display.V1.FormattedValue"];
+        var pageTitle = CompanyName;
+        if (cid_sitename != null && cid_sitename != "") {
+            if (cid_sitename.toLowerCase().indexOf("site") < 0) {
+                cid_sitename = cid_sitename + " Site";
+            }
+            pageTitle = pageTitle + " - " + cid_sitename;
+          }
+          else
+          {
+              var sitefulladdress ; 
+              //if the address is postal
+             if( ovs_address_type == 0)
+             {
+                 sitefulladdress = address1_line1 + ", " + address1_city + ", " + address1_stateorprovince + " Site";
+                 pageTitle = pageTitle + " - " +  sitefulladdress ;
+
+             }
+             else if (ovs_address_type == 1)
+             {
+                 if (ovs_lld_quarter != null)
+                 {
+                     sitefulladdress =  SiteAddressInfo[0]["ovs_lld_quarter@OData.Community.Display.V1.FormattedValue"] + " - " ;
+                 }
+                 sitefulladdress = sitefulladdress  + ovs_lld_section + " - " +  
+                 ovs_lld_township + " - " + ovs_lld_range + " - " + ovs_lld_meridian  + " Site" ;
+                 pageTitle = pageTitle + " - " +  sitefulladdress ;              
+             }
+             else if (ovs_address_type  == 2)
+             {
+                 sitefulladdress = "Latitude: " + address1_longitude + " Longitude: " + address1_latitude + " Site" ;
+                 pageTitle = pageTitle + " - " +  sitefulladdress ; 
+             }
+
+          }
+         const titleElement = document.getElementsByClassName("tab-title");
+         console.log("before title");
+         titleElement[0].innerHTML = pageTitle ;
+         const TDGActivitiesHeaderElement = document.querySelector('[aria-label="TDG Activity Types"]');
+         const ClassesHeaderElement = document.querySelector('[ aria-label="Classes"]');
+         TDGActivitiesHeaderElement.children[0].innerHTML = "TDG Activity Types for " + pageTitle ;
+         ClassesHeaderElement.children[0].innerHTML = "Class / Divisions for " + pageTitle ;
+         var ModeTable = document.getElementById('siteModesOfTransportation');
+         ModeTable.closest('fieldset').children[0].innerHTML = "Modes of Transportation for " + pageTitle ;
+        
+
+    }
+
+
+
+
     document.getElementById("address1_latitude").addEventListener('change', (event) => {
     //get Latitude and set it to nearest 4 digits
     var Lat = $("#address1_latitude").val();
