55735828176054aaf40632a48d6816cdd9fc2fcf
tc-buildbot <nepasrepondre-noreply@tc.gc.ca>
2023-02-17 15:57:51 +0000

    Update portal

--- Portals/cid-dev/basic-forms/company-naics-code---create/Company-NAICS-Code---Create.basicform.custom_javascript.js
+++ Portals/cid-dev/basic-forms/company-naics-code---create/Company-NAICS-Code---Create.basicform.custom_javascript.js
@@ -26,9 +26,12 @@ $(document).ready(function () {
 
 	debugger;
 	var cancelLabel = tdg.error_message.message("BTN_CANCEL");
-    $('#btn_save_new').after('<div role="group" class="btn-group entity-action-button">&nbsp;</div><input type="button" data-dismiss="modal" value="'+cancelLabel+'" id="CancelButton" name="CancelButton" class="submit-btn btn btn-primary form-action-container-left"/>')
-	$('#CancelButton').on('click', function(event){parent.$(".form-close").eq(0).click();});
+    //$('#btn_save_new').after('<div role="group" class="form-action-container-left"> &nbsp; </div><input type="button" data-dismiss="modal" value="'+cancelLabel+'" id="CancelButton" name="CancelButton" class="submit-btn btn btn-primary form-action-container-left"/>')
+	//$('#btn_save_new').after('<div role="group" class="form-action-container-left"> &nbsp; </div><input type="button" data-dismiss="modal" value="'+cancelLabel+'" id="CancelButton" name="CancelButton" class="btn btn-default"/>')
 
+
+    //$('#CancelButton').on('click', function(event){debugger; parent.$('.form-close').eq(0).click();});  
+    //$('#CancelButton').on('click', function(event){$('.modal').remove();});
 	//Test The NAICS Code must be selected using the NAICS Code drop-down. Enter at least the first two digits or your full NAICS Code to filter the drop-down and select the applicable NAICS Code from the list.</p>");
 });
 
