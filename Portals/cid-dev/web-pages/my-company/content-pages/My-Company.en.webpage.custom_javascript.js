71323395e20d4aa9a2b14146ddd3cfbf158f9b24
tc-buildbot <nepasrepondre-noreply@tc.gc.ca>
2023-03-27 20:06:35 +0000

    Update portal

--- Portals/cid-dev/web-pages/my-company/content-pages/My-Company.en.webpage.custom_javascript.js
+++ Portals/cid-dev/web-pages/my-company/content-pages/My-Company.en.webpage.custom_javascript.js
@@ -25,9 +25,10 @@ $(document).ready(function () {
 
     var cancelLabel = tdg.error_message.message("BTN_CANCEL");
     var updateCompanyBtn = "<div><input id='update_company' type='button' name='UpdateCompany' value='Update Organization' class='btn btn-primary action create-action' nonactionlinkbutton='true'/></div>"
-   // $(".form-custom-actions").first().parent().after(updateCompanyBtn);
-    $("#EntityFormPanel").append(updateCompanyBtn);
-   // $('div[data-name="company_details_section_3"]').after(updateCompanyBtn);
+    //$(".form-custom-actions").first().parent().append(updateCompanyBtn);
+    // $(".form-custom-actions").append(updateCompanyBtn);
+   // $("#EntityFormPanel").append(updateCompanyBtn);
+   $('table[data-name="company_details_section_3"]').after(updateCompanyBtn);
   
 
     var cancelBtn = "&nbsp;<input id='cancel_company_update' type='button' name='CancelCompanyUpdate' value='" + cancelLabel + "' class='btn btn-default button previous previous-btn' nonactionlinkbutton='true'/>";
