2190f98f1e668e568bd1560c0906480407890352
tc-buildbot <nepasrepondre-noreply@tc.gc.ca>
2023-08-09 22:00:37 +0000

    Update Solution - TC_GlobalCore:

--- Solutions/TC_GlobalCore/unpack/WebResources/cid_historylog.js
+++ Solutions/TC_GlobalCore/unpack/WebResources/cid_historylog.js
@@ -4,4 +4,26 @@ function Me_OnLoad(eContext)
     var lang = Xrm.Page.context.getUserLcid();
     var field = (lang == 1033 ? "cid_memoenglish" : "cid_memofrench");
     Xrm.Page.getControl(field).setVisible(true);
+
+   //show hide history code description
+
+   //get the quick view control
+   var quickViewControlEnglish =Xrm.Page.ui.quickForms.get("QuickviewControl_EnglishDescription");
+  var quickViewControlFrench = Xrm.Page.ui.quickForms.get("QuickviewControlFrenchDescripton");
+	
+  //hide the quick view
+  if (lang == 1033 )
+  {  
+          quickViewControlEnglish.setVisible(true);
+          quickViewControlFrench.setVisible(false);
+  }
+  else
+   {
+          quickViewControlEnglish.setVisible(false);
+          quickViewControlFrench.setVisible(true);
+
+   }
+
+
+
 }
