f88cf8d6393b5bf7bc85b87d3da2aed50fcba44d
tc-buildbot <nepasrepondre-noreply@tc.gc.ca>
2023-09-14 22:02:18 +0000

    Update portal

--- Portals/cid-dev/web-pages/access-denied/content-pages/Access-Denied.en.webpage.custom_javascript.js
+++ Portals/cid-dev/web-pages/access-denied/content-pages/Access-Denied.en.webpage.custom_javascript.js
@@ -1,9 +1,10 @@
 //
 // Web Page-Access Denied
 // 
+
 $(document).ready(function () {
 	debugger;
-	
+
 	// page_setup();
 });
 
@@ -36,7 +37,6 @@ page_setup();
 //		var formGroup = $(label).parent();
 //		$(formGroup).after('<div class="form-group"><label class="col-sm-2 control-label" for="ConfirmEmail"><div class="xrm-editable-text xrm-attribute"><div class="xrm-attribute-value-encoded xrm-attribute-value">Confirm Email</div></div></label><div class="col-sm-10"><input class="form-control" data-val="true" data-val-required="Confirm Email field is required." id="ConfirmEmail" name="ConfirmEmail" type="text" value=""><p class="help-block"><span class="xrm-editable-text xrm-attribute"><span class="xrm-attribute-value-encoded xrm-attribute-value">Re-enter email address.</span></span></p></div></div>');
 
-
 if ($('.validation-summary-errors').length) {
 	var InnerText = $('.validation-summary-errors')[0].innerText;
 	//fix special char issue
@@ -63,7 +63,6 @@ if (document.getElementsByTagName('h1')[0]) {
 		$(":button.btn.btn-primary.btn-line")[0].title = signin_ttip;
 	}
 
-	
 	var source = document.getElementsByTagName('h1')[0].innerHTML;
 	var terms_and = tdg.error_message.message("m000199");
 	var terms_and_conditions = tdg.error_message.message("m000189");
@@ -88,7 +87,7 @@ if (document.getElementsByTagName('h1')[0]) {
 $('legend').after("<br><br><br><br>");
 //disabled register key if email is not entered on the change event of the text box
 $("#Email").on("change", function () {
-	if ($("#Email").val().trim() == "")  {
+	if ($("#Email").val().trim() == "") {
 		$(".btn-primary").prop('disabled', true);
 	}
 	else {
@@ -132,23 +131,27 @@ catch (e) {
 }
 if (text == Register_external_account) {
 	debugger;
-	
+
 	if ($('.validation-summary-errors')[0]) {
 		var Innerhtml = $('.validation-summary-errors')[0].innerHTML;
 		var InnerText = $('.validation-summary-errors')[0].innerText;
 		var The_email = tdg.error_message.message("m000192");
 		var invalid_invitation = tdg.error_message.message("m000194");
-	
-		if (Innerhtml != null && Innerhtml.substring(0, 18) == The_email) {
+
+		var email = $("#Email")[0].value;
+		var email_taken = InnerText.contains(email);
+
+		if (Innerhtml != null && email_taken) {
 			var email_in_use = tdg.error_message.message("m000193");
 			$('.validation-summary-errors')[0].innerHTML = email_in_use;
+
 			// popup msg
 			var email = $("#Email")[0].value;
 			var filter = "statecode eq 0 and emailaddress1 eq '" + email + "'";
 			var data = tdg.webapi.list("contacts", filter);
 			var parentcustomerid = data[0]._parentcustomerid_value;
 			if (parentcustomerid != null) {
-				var filter = "accountid eq " + parentcustomerid ;
+				var filter = "accountid eq " + parentcustomerid;
 				var data = tdg.webapi.list("accounts", filter);
 				var customertypecode = data[0].customertypecode;
 				switch (customertypecode) {
