63ca32a839e1b363a5bb3c85dc07155ab63ec38f
tc-buildbot <nepasrepondre-noreply@tc.gc.ca>
2024-06-18 06:10:03 +0000

    Update Solution - TC_GlobalCore_Patch_7260c04f:

---
+++ Solutions/TC_GlobalCore/Patches/TC_GlobalCore_Patch_7260c04f/unpack/WebResources/ovs_DGAIS_MOC_MainForm.js
@@ -0,0 +1,69 @@
+﻿///<reference path="../../Utilities/GlobalHelper.js"/>
+
+
+var DGAIS_MOC_MainForm = (function (window, document) {
+    //variables
+    var formType;
+    //********************private methods*******************
+
+
+    //********************private methods end***************
+
+    //********************public methods***************
+    return {
+
+        OnLoad: async function (executionContext) {
+
+            var formContext = executionContext.getFormContext();
+            formType = glHelper.GetFormType(formContext);
+
+            var release = formContext.getAttribute("ovs_release_cd");
+            release.removeOnChange(DGAIS_MOC_MainForm.Release_OnChange);
+            release.addOnChange(DGAIS_MOC_MainForm.Release_OnChange);
+            release.fireOnChange();
+
+            var releaseLocation = formContext.getAttribute("ovs_release_location_mcd");
+            releaseLocation.removeOnChange(DGAIS_MOC_MainForm.ReleaseLocation_OnChange);
+            releaseLocation.addOnChange(DGAIS_MOC_MainForm.ReleaseLocation_OnChange);
+            releaseLocation.fireOnChange();
+
+
+            if (formType == glHelper.FORMTYPE_CREATE) {
+
+
+            }
+            else {
+
+ 
+            }
+        },
+
+        ReleaseLocation_OnChange: async function (executionContext) {
+            try {
+                var formContext = executionContext.getFormContext();
+                glHelper.openOtherFromChoice_s(formContext, "ovs_release_location_mcd", "918640019", "ovs_release_location_other_txt");
+
+            } catch (error) {
+                Xrm.Navigation.openErrorDialog({ message: error })
+
+            } finally {
+            }
+        },
+
+        Release_OnChange: function (executionContext) {
+
+            var formContext = executionContext.getFormContext();
+
+            var isRelease = formContext.getAttribute("ovs_release_cd").getValue();
+
+            if (!isRelease)
+                formContext.getAttribute("ovs_release_location_mcd").setValue([]);
+            glHelper.SetDisabled(formContext, "ovs_release_location_mcd", !isRelease);
+            glHelper.SetRequiredLevel(formContext, "ovs_release_location_mcd", isRelease);
+
+        },
+
+  
+    };
+    //********************public methods end***************
+})(window, document);
