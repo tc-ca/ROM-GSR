18edb640ff65503aba45f7291d3f0d613f17393a
tc-buildbot <nepasrepondre-noreply@tc.gc.ca>
2024-09-10 18:14:45 +0000

    Update Solution - TC_GlobalCore_Patch_7797a52c:

---
+++ Solutions/TC_GlobalCore/Patches/TC_GlobalCore_Patch_7797a52c/unpack/WebResources/ovs_Incident_MainForm.js
@@ -0,0 +1,200 @@
+﻿///<reference path="../../Utilities/GlobalHelper.js"/>
+///<reference path="../../Utilities/questionnaireFunctions.js"/>
+
+
+var TDG_Incident_MainForm = (function (window, document) {
+    //variables
+    var formType;
+    var facilityTypeExcluded = new Array(1, 12, 15, 19, 22);
+
+    var PostalAdressArray = new Array("ovs_street1_txt", "ovs_street2_txt", "ovs_street3_txt", "ovs_city_txt", "ovs_state_province_txt", "ovs_zip_postalcode_txt", "ovs_country_txt");
+    var LongLatArray = new Array("ovs_latitude_num", "ovs_longitude_num");
+    var LLD_Array = new Array("ovs_quarter_lsd_cd", "ovs_range_cd", "ovs_section_cd", "ovs_township_cd");
+
+
+    //********************private methods*******************
+
+    function clearHideEase(formContext, arrayOfFields, doClearHideEase) {
+
+        for (var i = 0; i < arrayOfFields.length; i++) {
+            if (doClearHideEase) glHelper.SetValue(formContext, arrayOfFields[i], null);
+            glHelper.SetControlVisibility(formContext, arrayOfFields[i], !doClearHideEase);
+            glHelper.SetRequiredLevel(formContext, arrayOfFields[i], !doClearHideEase);
+        }
+    }
+
+    function filterFacilityType(formContext) {
+
+        glHelper.filterOptionSet(formContext, "ovs_facility_type_cd", facilityTypeExcluded, false);
+    }
+
+    //********************private methods end***************
+
+    //********************public methods***************
+    return {
+
+        OnLoad: function (executionContext) {
+
+            var formContext = executionContext.getFormContext();
+            formType = glHelper.GetFormType(formContext);
+
+            filterFacilityType(formContext);
+
+            var source = formContext.getAttribute("ovs_source_cds");
+            source.removeOnChange(TDG_Incident_MainForm.Source_OnChange);
+            source.addOnChange(TDG_Incident_MainForm.Source_OnChange);
+            source.fireOnChange();
+
+            var phImpact = formContext.getAttribute("ovs_physical_impact_cds");
+            phImpact.removeOnChange(TDG_Incident_MainForm.PhisicalImpact_OnChange);
+            phImpact.addOnChange(TDG_Incident_MainForm.PhisicalImpact_OnChange);
+           
+            var phImpactFlag = formContext.getAttribute("ovs_physical_impact_flag_ind");
+            phImpactFlag.removeOnChange(TDG_Incident_MainForm.PhisicalImpactFlag_OnChange);
+            phImpactFlag.addOnChange(TDG_Incident_MainForm.PhisicalImpactFlag_OnChange);
+            phImpactFlag.fireOnChange();
+
+            var contributingFactors = formContext.getAttribute("ovs_contributing_factors_cds");
+            contributingFactors.removeOnChange(TDG_Incident_MainForm.ContributingFactors_OnChange);
+            contributingFactors.addOnChange(TDG_Incident_MainForm.ContributingFactors_OnChange);
+
+            var addressDetementFlag = formContext.getAttribute("ovs_address_not_determent_ind");
+            addressDetementFlag.removeOnChange(TDG_Incident_MainForm.AddressDeterment_OnChange);
+            addressDetementFlag.addOnChange(TDG_Incident_MainForm.AddressDeterment_OnChange);
+            addressDetementFlag.fireOnChange();
+
+
+            //initiate Copy Address control
+            var wrControl = formContext.getControl("WebResource_CopyButton");
+            if (wrControl) wrControl.getContentWindow().then(function (contentWindow) {contentWindow.setClientApiContext(Xrm, formContext);})  
+
+            if (formType == glHelper.FORMTYPE_CREATE) {
+
+
+            }
+            else {
+                var seriouseJeopardy = formContext.getAttribute("ovs_serious_jeopardy_ind");
+                seriouseJeopardy.removeOnChange(TDG_Incident_MainForm.Jeopardy_OnChange);
+                seriouseJeopardy.addOnChange(TDG_Incident_MainForm.Jeopardy_OnChange);
+
+                var exceptionExemtion = formContext.getAttribute("ovs_exemption_ind");
+                exceptionExemtion.removeOnChange(TDG_Incident_MainForm.Exemtion_OnChange);
+                exceptionExemtion.addOnChange(TDG_Incident_MainForm.Exemtion_OnChange);
+            }
+        },
+
+        Source_OnChange: function (executionContext) {
+
+            var formContext = executionContext.getFormContext();
+
+            glHelper.openOtherFromChoice_s(formContext, "ovs_source_cds", "23", "ovs_other_source_txt");
+        },
+
+        PhisicalImpact_OnChange: function (executionContext) {
+
+            var formContext = executionContext.getFormContext();
+
+            glHelper.openOtherFromChoice_s(formContext, "ovs_physical_impact_cds", "9", "ovs_other_physical_impact_txt");
+        },
+
+        PhisicalImpactFlag_OnChange: function (executionContext) {
+
+            var formContext = executionContext.getFormContext();
+
+            var flagOn = formContext.getAttribute("ovs_physical_impact_flag_ind").getValue();
+            if (!flagOn) glHelper.SetValue(formContext, "ovs_physical_impact_cds", null);
+            glHelper.SetControlReadOnly(formContext, "ovs_physical_impact_cds", !flagOn);
+            //glHelper.SetControlVisibility(formContext, "ovs_physical_impact_cds", flagOn);
+            TDG_Incident_MainForm.PhisicalImpact_OnChange(executionContext);
+        },
+
+        ContributingFactors_OnChange: function (executionContext) {
+
+            var formContext = executionContext.getFormContext();
+
+            glHelper.openOtherFromChoice_s(formContext, "ovs_contributing_factors_cds", "9", "ovs_contributing_factor_other_txt");
+        },
+
+        AddressType_OnChange: function (executionContext) {
+
+            var formContext = executionContext.getFormContext();
+
+            switch (formContext.getAttribute("ovs_address_type_cd").getValue()) {
+
+                //Postal Address
+                case 0: {
+                    //clean and hide LongLat, LLD, enabel Postal
+                    clearHideEase(formContext, LongLatArray, true);
+                    clearHideEase(formContext, PostalAdressArray, false);
+                    clearHideEase(formContext, LLD_Array, true);
+
+                    //street2 and 3 are never required
+                    glHelper.SetRequiredLevel(formContext, "ovs_street2_txt", false);
+                    glHelper.SetRequiredLevel(formContext, "ovs_street3_txt", false);
+                } break;
+                case 1: {
+                    //clean and hide Postal Adress fields, LongLat, enable LLD
+                    clearHideEase(formContext, LongLatArray, true);
+                    clearHideEase(formContext, PostalAdressArray, true);
+                    clearHideEase(formContext, LLD_Array, false);
+                } break;
+                //LongLat
+                case 2: {
+                    //clean and hide Postal Adress fields,LLD, enable LongLat
+                    clearHideEase(formContext, LongLatArray, false);
+                    clearHideEase(formContext, PostalAdressArray, true);
+                    clearHideEase(formContext, LLD_Array, true);
+                } break;
+                default: {
+                    //clean and hide all
+                    clearHideEase(formContext, LongLatArray, true);
+                    clearHideEase(formContext, PostalAdressArray, true);
+                    clearHideEase(formContext, LLD_Array, true);
+                } break;
+            }
+        },
+
+        AddressDeterment_OnChange: function (executionContext) {
+
+            var formContext = executionContext.getFormContext();
+            var isNotDeterment = glHelper.GetValue(formContext, "ovs_address_not_determent_ind");
+            var addressType = formContext.getAttribute("ovs_address_type_cd");
+
+            //set section visibility
+            glHelper.SetSectionVisibility(formContext, "tab_5", "tab_5_section_addressAuto", !isNotDeterment);
+            glHelper.SetSectionVisibility(formContext, "tab_5", "tab_5_section_siteAddress", !isNotDeterment);
+            glHelper.SetSectionVisibility(formContext, "tab_5", "tab_5_section_addressManual", isNotDeterment);
+            
+            //set fields required
+            for (var i = 0; i < PostalAdressArray.length; i++) {
+                glHelper.SetRequiredLevel(formContext, PostalAdressArray[i], !isNotDeterment);
+            }
+
+            if (isNotDeterment) {
+
+                addressType.removeOnChange(TDG_Incident_MainForm.AddressType_OnChange);
+            }//determent
+            else {
+
+                addressType.removeOnChange(TDG_Incident_MainForm.AddressType_OnChange);
+                addressType.addOnChange(TDG_Incident_MainForm.AddressType_OnChange);
+                addressType.fireOnChange();
+            }
+
+        },
+
+        Jeopardy_OnChange: function (executionContext) {
+
+            var formContext = executionContext.getFormContext();
+            glHelper.DisplayFormNotificationModern(formContext, "Change of 'Serious Jeopardy' may affect 'Regulatory Scope' calculation. Please, recalculate the scope.", "WARNING", true, 50000);
+        },
+
+        Exemtion_OnChange: function (executionContext) {
+
+            var formContext = executionContext.getFormContext();
+            glHelper.DisplayFormNotificationModern(formContext, "Change of 'Exception/Exemption' may affect 'Reportable' status  calculation. Please, recalculate the scope.", "WARNING", true, 50000);
+        },
+
+    };
+    //********************public methods end***************
+})(window, document);
---
+++ Solutions/TC_GlobalCore/Patches/TC_GlobalCore_Patch_7797a52c/unpack/WebResources/ovs_incidentReport_ribbon.js
@@ -0,0 +1,141 @@
+﻿var irRibbon = (function (window, document) {
+
+    function promptUserCalc(irId, incidentId, LCID, gridControl, formContext) {
+
+
+        if (formContext.data.entity.getIsDirty()) {
+
+            var confirmStrings = { text: "Form data was modifiied. The form will be saved prior to the calculation begins. Continue?", title: "Confirmation Dialog" };
+            var confirmOptions = { height: 200, width: 450 };
+            Xrm.Navigation.openConfirmDialog(confirmStrings, confirmOptions).then(
+                function (success) {
+                    if (success.confirmed) {
+
+                        //async save
+                        formContext.data.save().then(function () {
+                            setPrimaryAndCalculate(irId, incidentId, LCID, gridControl, formContext);
+                        }, function (e) {
+
+                            Xrm.Navigation.openAlertDialog({ confirmButtonLabel: "OK", text: "Cannot save the form. Error: " + this.statusText });
+
+                        });
+                    }
+                    else {
+
+                        return;
+                    }
+                }
+            );
+
+        }
+        else setPrimaryAndCalculate(irId, incidentId, LCID, gridControl, formContext);        
+    }
+
+    function setPrimaryAndCalculate(irId, incidentId, LCID, gridControl, formContext) {
+
+        //TO DO: localize
+        Xrm.Utility.showProgressIndicator("Calculating...");
+
+        //call custom action to calculate
+        var parameters = {};
+        parameters.incidentId = incidentId.replace('{', '').replace('}', '');
+        parameters.irId = irId.replace('{', '').replace('}', '');
+
+        var ovs_CalculateIncidentScopeRequest = {
+            incidentId: parameters.incidentId,
+            irId: parameters.irId,
+
+            getMetadata: function () {
+                return {
+                    boundParameter: null,
+                    parameterTypes: {
+                        "incidentId": {
+                            "typeName": "Edm.String",
+                            "structuralProperty": 1
+                        },
+                        "irId": {
+                            "typeName": "Edm.String",
+                            "structuralProperty": 1
+                        }
+                    },
+                    operationType: 0,
+                    operationName: "ovs_CalculateIncidentScope"
+                };
+            }
+        };
+
+        Xrm.WebApi.online.execute(ovs_CalculateIncidentScopeRequest).then(
+            function success(result) {
+
+                Xrm.Utility.closeProgressIndicator();
+
+                if (result.ok) {
+                    result.text().then(function (i) {
+                        var data = JSON.parse(i);
+                        if (data != null || typeof data !== 'undefined') {
+
+                            //refresh form or grid
+                            if (data.isOK) {
+
+                                //refresh without save
+                                formContext.data.refresh(false).then(function (success) {
+
+                                    if (gridControl != null) {
+
+                                        var gridContext = formContext.getControl("Subgrid_reports");
+                                        gridContext.refresh();
+                                    }
+                                }, function (error) {
+                                    console.log("Refresh error: " + error.message);
+                                    Xrm.Navigation.openErrorDialog({ message: "Something went wrong with form refresh. Error: " + error.message });
+
+                                });
+                            }
+                            else {
+
+                                Xrm.Navigation.openErrorDialog({ message: "Calculation went wrong. Error: " + data.executionError });
+                            }
+                        }
+                    }, function (error) {
+
+                        Xrm.Utility.closeProgressIndicator();
+                        console.log("Regs error: " + error.message);
+                        Xrm.Navigation.openErrorDialog({ message: "Something went wrong. Error: " + error.message });
+                    });
+                }
+            },
+            function (error) {
+
+                Xrm.Utility.closeProgressIndicator();
+                Xrm.Navigation.openErrorDialog({ message: error.message });
+            }
+        );
+    }
+
+    function incidentFormGrid(irId, incidentId, LCID, gridControl, formConrol) {
+
+        var formContext = formConrol;//incident form
+
+        promptUserCalc(irId, incidentId, LCID, gridControl, formContext);
+    }
+
+    function incidentReportForm(irId, LCID, formConrol) {
+
+        var formContext = formConrol;//incident report form
+
+        //get incidentId
+        incidentId = formContext.getAttribute("ovs_incident_id").getValue()[0].id;
+
+        promptUserCalc(irId, incidentId, LCID, null, formContext);
+
+    }
+
+    //********************public methods***************
+
+    return {
+
+        incidentFormGrid: incidentFormGrid,
+        incidentReportForm: incidentReportForm
+    };
+
+})(window, document);
